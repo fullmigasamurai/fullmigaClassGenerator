737c7535e598b8731186ea17556ebf9255009fa8
fullmigasamurai <fullmigasamurai@users.noreply.github.com>
2020-09-08 19:28:45 -0400

    fufile version 2

--- src/main/java/classGenerator/FuFile.java
+++ src/main/java/classGenerator/FuFile.java
@@ -4,50 +4,24 @@ import java.io.File;
 
 public class FuFile {
 
-	private File file;
-
-	public FuFile(String parent, String child) {
-		this.file = new File(parent, child);
-		
-	}
-	public FuFile(String pathname) {
-		this.file = new File(pathname);
-	}
-	public FuFile(File parent, String child) {
-		this.file = new File(parent, child);
-	}
-
-	public void setName (String name) {
-		this.file = new File (file.getParent()+name);
-	}
-
-	public File changeName (String oldName, String newName) {
-		if (this.file.getName().contains("oldName")){
-			this.file = new File(this.file.getParent()+newName);
+	public File changeName (File file, String newName) {
+		if (file.getName().contains(newName)){
+			file = new File(file.getParent()+newName);
 		}
-		return this.file;
+		return file;
 	}
 
-	public boolean replaceFile (String oldName, String newName){
-		if (this.file.getName().contains("oldName")){
-			File replace = new File(this.file.getParent()+newName);
-			Boolean b = this.file.renameTo(replace);
-			return b;
+	public File replaceFile (File file, String newName){
+		if (file.getName().contains(newName)){
+			File replace = new File(file.getParent()+newName);
+			Boolean b = file.renameTo(replace);
+
+			return b ? replace : null;
+
 		} else {
-			return false;
+			return file;
 		}
 
 	}
-
-	public File getFile ()
-	{
-		return this.file;
-	}
-
-
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 1L;
 	
 }
